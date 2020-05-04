7621ebb93295e05251d239a0ebcbdb9e7560e3b4
marquake <marcosms@gmail.com>
2020-05-04 10:39:49 +0200

    primero commit de aplicación

---
+++ appTest3Spring/src/main/java/com/springweb/app/AppTest3SpringApplication.java
@@ -0,0 +1,13 @@
+package com.springweb.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTest3SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTest3SpringApplication.class, args);
+	}
+
+}
